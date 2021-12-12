5c8ffc04077d4a37eb5e2bbea93fa45d727f4e0e
Jeff Ripke <jripke74@gmail.com>
2021-12-12 15:13:14 -0800

    add basic layout and components

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,22 @@
 import React from "react";
+import Tilt from "react-tilt";
+import brain from "./brain.png";
+import "./Logo.css";
 
 const Logo = () => {
   return (
     <div className="ma4 mt0">
-
+      <Tilt
+        className="Tilt br2 shadow-2"
+        option={{ max: 25 }}
+        style={{ height: 150, width: 150 }}
+      >
+        <div className="Tilt-inner">
+          <img style={{ paddingTop: "0" }} alt="logo" src={brain} />
+        </div>
+      </Tilt>
     </div>
   );
-}
+};
 
 export default Logo;
